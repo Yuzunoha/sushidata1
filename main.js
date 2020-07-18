cc634813bf05b5a82e32cb5fd327d7506e1546d5
Yuzunoha <studysteadystandby@gmail.com>
2020-07-18 21:23:30 +0900

    サンプルコードを記載したぞ

--- main.js
+++ main.js
@@ -17,22 +17,10 @@ const createObjectFromLine = (line) => {
 const datastr = fs.readFileSync('datastr.txt', 'utf8');
 const dataArr = datastr.split('\n');
 
-// sample start 古い順
-var arr = [
-  {
-    date: '2015/06/14 20:02:02',
-    content: 'g',
-  },
-  {
-    date: '2014/06/14 20:02:01',
-    content: 'f',
-  },
-  {
-    date: '2015/05/12 10:02:02',
-    content: 'e',
-  },
-];
-arr.sort((a, b) => (a.date > b.date ? 1 : -1)); // 古い順
-// sample end 古い順
+// オブジェクトのディープコピー
+// const b = JSON.parse(JSON.stringify(a));
+
+// 日付が古い順にソート
+// b.sort((a, b) => (a.date > b.date ? 1 : -1));
 
 p(createObjectFromLine(dataArr[77]));
