06fb67c42f7b33b14466363029cf01cc5c132d93
Yuzunoha <studysteadystandby@gmail.com>
2020-07-18 21:03:55 +0900

    オブジェクト化成功

--- main.js
+++ main.js
@@ -2,7 +2,19 @@
 
 const fs = require('fs');
 const p = console.log;
+const createObjectFromLine = (line) => {
+  const a = line.trim().split(' ');
+  const o = {};
+  o.rank = Number(a[0].split('位')[0]);
+  o.name = a[1];
+  o.score = Number(a[2]);
+  o.typeCnt = Number(a[4]);
+  o.typeCntAve = Number(a[6]);
+  o.typeMissCnt = Number(a[8]);
+  o.created = a[10];
+  return o;
+};
 const datastr = fs.readFileSync('datastr.txt', 'utf8');
 const dataArr = datastr.split('\n');
 
-p(dataArr);
+p(createObjectFromLine(dataArr[77]));
